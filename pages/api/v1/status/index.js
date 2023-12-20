1694fac8c53897f630d211ade756bd26877132fb
André <90482435+andrencor@users.noreply.github.com>
2023-12-20 18:36:13 +0000

    Finalização do endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  let result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ status: "testando utf são é à" });
+  let updatedAt = new Date().toISOString();
+
+  let dbMaxConnections = await database.query("SHOW max_connections;");
+  dbMaxConnections = dbMaxConnections.rows[0].max_connections;
+
+  let dbOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  dbOpenedConnections = dbOpenedConnections.rows[0].count;
+
+  let dbVersion = await database.query("SHOW server_version;");
+  dbVersion = dbVersion.rows[0].server_version;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: dbOpenedConnections,
+        version: dbVersion,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
-test("GET /api/v1/status (Expected: 200)", async () => {
+test("GET /api/v1/status", async () => {
   let response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  let responseBody = await response.json();
+
   expect(response.status).toBe(200);
+
+  let parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
